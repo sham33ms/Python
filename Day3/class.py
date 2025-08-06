aedacbeefffb8afd71671a93f0d1fda1bc28a96c
shameem <shameem4contact@gmail.com>
2025-08-06 11:57:52 +0530

    Day3

---
+++ Day3/class.py
@@ -0,0 +1,17 @@
+#without class
+user1_name = "Alice"
+user1_email = "alice@example.com"
+user1_posts = ["Hello World!"]
+
+user2_name = "Bob"
+user2_email = "bob@example.com"
+user2_posts = ["My first post."]
+
+def login_user(name):
+    print(f"{name} has logged in.")
+
+def post_message(name, message):
+    # a real function would need to find the right post list
+    print(f"{name} posted: {message}")
+
+login_user(user1_name) # Alice has logged in.
--- pandas/panda1.py
+++ pandas/panda1.py
@@ -1,23 +1,29 @@
 import pandas as pd
 
-list1=[1,2,3,4,5,6]
-l1=pd.Series(list1)
-print(l1)
+# list1=[1,2,3,4,5,6]
+# l1=pd.Series(list1)
+# print(l1)
 
 s1=[98,77,88,87,86,89]
+student_scores = {
+    'Alice': 95,
+    'Bob': 88,
+    'Charlie': 92,
+    'David': 78
+}
 s2=["Rishi", "Shameem","Raj","Raaja","Britto", "Dervin"]
 s3=pd.Series(s1,index=s2)
 print(s3)
 print(s3.index)
 print(s3.values)    
 
-s4= pd.Series(data=[85, 92, 78, 65, 100], index=['Alice', 'Bob', 'Charlie', 'David', 'Eve'])
+s4= pd.Series([85, 92, 78, 65, 100], index=['Alice', 'Bob', 'Charlie', 'David', 'Eve'])
 print(s4)
 
 print(s4['Bob'])
 
 print(s4[['Alice', 'Eve']])
-print(s4.loc['Bob']) 
+
 # add=s4+5
 print(s4+5)
 
@@ -26,3 +32,11 @@ print(s4/100)
 # passing_mask = s4 > 80
 # print("This is the boolean mask:")
 print(s4 > 80)
+
+
+# #dict
+
+
+scores_series = pd.Series(student_scores)
+
+print(scores_series)
